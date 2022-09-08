84b32592d818556e4b62875211ba1b9ceefac3cc
Kashyap <kashyaprishi208@gmail.com>
2022-09-08 23:52:13 +0530

    Done pitch page

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -1,8 +1,22 @@
+
+
+
 import React from "react";
 import styles from "../styles/ContentSection.module.css";
 
 const ContentSection = () => {
-  return <div className={styles.ContentSection}>ContentSection</div>;
+  return(
+    <>
+    <div className={styles.ContentSection}>
+  <h1 className={styles.head1}>Pitch</h1>
+<hr/>
+  
+   <div className={styles.content1}>
+    laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores.
+    </div>
+  </div>
+  </>
+  );
 };
 
 export default ContentSection;
