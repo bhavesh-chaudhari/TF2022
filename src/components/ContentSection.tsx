03acef83064387c504a9254941dbfb52b7ed2f32
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2022-09-09 00:20:41 +0530

    separate content and about component

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -3,9 +3,11 @@ import styles from "../styles/ContentSection.module.css";
 import AboutUs from "./AboutUs";
 
 const ContentSection = () => {
-  return <div className={styles.ContentSection}>
-    <AboutUs />
-  </div>;
+  return (
+    <div className={styles.ContentSection}>
+      <AboutUs />
+    </div>
+  );
 };
 
 export default ContentSection;
