7d65e0570e0f7294f2f44be9ce544b815e945029
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-09 21:59:44 +0530

    correct the css of highlights component

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -3,21 +3,24 @@ import styles from "../styles/Highlights.module.css";
 import ReactPlayer from 'react-player'
 
 const Highlights = () => {
-  return <div className={styles.Highlights}>
+  return (
+    <div className={styles.HighlightsSection}>
+        <div>
+            <h1 className={styles.Heading}>Highlights</h1>
+            <div className={styles.underline} />
+        </div>
+        <div className={`${styles.playerWrapper}`}>
+            <ReactPlayer className={styles.player} width="100%" url="https://www.youtube.com/watch?v=0v4iftGneYk&ab_channel=TantraFiestaIIITNagpur" controls={true}/>
+        </div>
+        <div className={styles.content}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Vel beatae quibusdam maiores obcaecati cumque consequuntur, 
+            quaerat sunt praesentium velit! Facilis sapiente possimus 
+            minima, perspiciatis iure odit enim dicta vel reiciendis.
+        </div>
 
-    <div className={styles.Cont}>
-        <div className={`${styles.Headd}  ${styles.Headline}`}>
-            <span>Highlights</span>
-        </div>
-        <div className={`${styles.Play}`}>
-            <ReactPlayer url="https://www.youtube.com/watch?v=0v4iftGneYk&ab_channel=TantraFiestaIIITNagpur" controls={true}/>
-        </div>
-        <div className={styles.Contet}>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel beatae quibusdam maiores obcaecati cumque consequuntur, quaerat sunt praesentium velit! Facilis sapiente possimus minima, perspiciatis iure odit enim dicta vel reiciendis.</p>
-        </div>
-  </div>
-
-  </div>;
+    </div>
+  );
 };
 
 export default Highlights;
