22db04b51a7bc75001eebc9b5c46c2d7c2d5ada2
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-09 22:01:31 +0530

    correction of styles of highlights component

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -3,10 +3,12 @@ import styles from "../styles/ContentSection.module.css";
 import Highlights from "./Highlights";
 
 const ContentSection = () => {
-  return <div className={styles.ContentSection}>
-    {/* {`${styles.class-name1} ${styles.class-name2}`} */}
-    <Highlights/>
-  </div>;
+  return (
+    <div className={styles.ContentSection}>
+      {/* {`${styles.class-name1} ${styles.class-name2}`} */}
+      <Highlights />
+    </div>
+  );
 };
 
 export default ContentSection;
--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import styles from "../styles/Highlights.module.css";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 const Highlights = () => {
   return (
     <div className={styles.HighlightsSection}>
-        <div>
-            <h1 className={styles.Heading}>Highlights</h1>
-            <div className={styles.underline} />
-        </div>
-        <div className={`${styles.playerWrapper}`}>
-            <ReactPlayer className={styles.player} width="100%" url="https://www.youtube.com/watch?v=0v4iftGneYk&ab_channel=TantraFiestaIIITNagpur" controls={true}/>
-        </div>
-        <div className={styles.content}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Vel beatae quibusdam maiores obcaecati cumque consequuntur, 
-            quaerat sunt praesentium velit! Facilis sapiente possimus 
-            minima, perspiciatis iure odit enim dicta vel reiciendis.
-        </div>
-
+      <div>
+        <h1 className={styles.Heading}>Highlights</h1>
+        <div className={styles.underline} />
+      </div>
+      <div className={`${styles.playerWrapper}`}>
+        <ReactPlayer
+          className={styles.player}
+          width="100%"
+          url="https://www.youtube.com/watch?v=0v4iftGneYk&ab_channel=TantraFiestaIIITNagpur"
+          controls={true}
+        />
+      </div>
+      <div className={styles.content}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel beatae
+        quibusdam maiores obcaecati cumque consequuntur, quaerat sunt
+        praesentium velit! Facilis sapiente possimus minima, perspiciatis iure
+        odit enim dicta vel reiciendis.
+      </div>
     </div>
   );
 };
