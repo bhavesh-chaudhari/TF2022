b61feca5593492f188ad327c82440dcbe37c5e7e
sanket <you@example.com>
2022-09-09 10:42:36 +0530

    changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,65 @@
+//@ts-nocheck
+import {useState} from "react";
 import React from "react";
 import styles from "../styles/Navbar.module.css";
-
 const Navbar = () => {
-return <div>
-    <div className={styles.MainNav}>
-       <div className={styles.elements}>
-        <ul>
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">ABOUT</a></li>
-        </ul>
-       </div>
-    </div>
-   </div>;
+  const [isActive, setIsActive]= useState(false);
+   const handleClick =(button)=>{
+    setIsActive(current=>!current);
+  
+   };
+  return (
+       <>
+      <div className={styles.MainNav}>
+        <div className={styles.elements}>
+          <ul>
+            <li className={styles.links} id="About">
+              <a href="#" onClick={handleClick}>ABOUT</a>
+            </li>
+            <li className={styles.links } id="Pitch">
+              <a href="#" onClick={handleClick}>HIGHLIGHTS</a>
+            </li>
+            <li className={styles.links} id="mfs">
+              <a href="#" onClick={handleClick}>MESSAGE FOR <br /> STUDENTS  </a>
+            </li>
+            <li className={styles.links} id="mfsn">
+              <a href="#" onClick={handleClick}>MESSAGE FOR <br /> SPONSORS </a>
+            </li>
+            <li className={styles.links} id="Team">
+              <a href="#" onClick={handleClick}>TEAM</a>
+            </li>
+          </ul>
+          </div>
+          <div className={styles.maincontainer}>
+          <div className={styles.container}>
+          </div>
+          <div className={styles.circles}>
+            <div className={styles.circle1}>
+                <div className={styles.circle2} id="AboutCircle" style={{backgroundColor:isActive?'green':'black'}}>
+                    
+                    </div>
+            </div>
+            <div className={styles.circle1}>
+                <div className={styles.circle2}  id="PitchCircle" style={{backgroundColor:isActive?'green':'black'}}></div>
+            </div><div className={styles.circle1}>
+                <div className={styles.circle2}  id="HighlightsCircle"style={{backgroundColor:isActive?'green':'black'}}></div>
+            </div><div className={styles.circle1}>
+                <div className={styles.circle2}id="TeamCircle"style={{backgroundColor:isActive?'green':'black'}}></div>
+            </div>
+            <div className={styles.circle1}>
+                <div className={styles.circle2} id="TeamCircle2"style={{backgroundColor:isActive?'green':'black'}}></div>
+            </div>
+          </div>
+          </div>
+     </div>
+    </>
+  );
 };
 
 export default Navbar;
+
+
+// about 
+// highlights
+// message for students
+// message for sponsors
