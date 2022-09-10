ec5d5ee71641268d552b45a9eefd30d01ecffc9b
Bhavesh <bhic2030@gmail.com>
2022-09-10 14:28:12 +0530

    add hero skeleton and basic styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,14 @@ const document = () => {
           href="https://fonts.gstatic.com"
           crossOrigin={"true"}
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
