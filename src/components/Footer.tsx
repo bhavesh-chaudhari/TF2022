44069de22896b0647f0626c44f6f0361cd08df50
Pinchu2002 <priyanshuchhabda3@gmail.com>
2022-09-10 16:09:17 +0530

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,53 +17,55 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
         <div className={styles.logo}>
-          <img src={iiitn.src} alt="iiitN" className={styles.logo} />
-          <img src={line.src} alt="iiitN" className={styles.logo} />
-          <img src={tf.src} alt="tf" className={styles.logo} />
+            <img src={iiitn.src} alt="iiitN" className={styles.logo1}/>
+            <img src={line.src} alt="line"className={styles.logo1}/>
+            <img src={tf.src} alt="tf" className={styles.logo1}/>
         </div>
       <div className={styles.social_container}>
         <h3 className={styles.h3}>
           Social Media
         </h3>
         <a href="#"
-        className={styles.youtube_social}>
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </a>
-      <a href="#"
-        className={styles.facebook_social}>
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-      <a href="#" className={styles.twitter_social}>
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
-      </a>
-      <a href="#"
-        className={styles.instagram_social}>
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
+        className={styles.social}>
+        <FontAwesomeIcon icon={faYoutube} size="2x" className={styles.youtube}/>
+        </a>
+        <a href="#"
+          className={styles.social}>
+          <FontAwesomeIcon icon={faFacebook} size="2x" className={styles.facebook}/>
+        </a>
+        <a href="#" className={styles.social}>
+          <FontAwesomeIcon icon={faTwitter} size="2x" className={styles.twitter}/>
+        </a>
+        <a href="#"
+          className={styles.social}>
+          <FontAwesomeIcon icon={faInstagram} size="2x" className={styles.instagram}/>
+        </a>
       </div>
       <div className={styles.contact_address}>
         <div className={styles.contact}>
             <div className={styles.ts}>
                 <h3 className={styles.h3}>Yash Kumar Sharma</h3>
-                <p>Technical Secretary SAC
-                    IIIT NAGPUR
-                    ts@iiitn.ac.in
-                    7011214319</p>
+                <p>Technical Secretary SAC</p>
+                <p>IIIT NAGPUR</p>
+                <p>ts@iiitn.ac.in</p>
+                <p>7011214319</p>
             </div>
             <div className={styles.gs}>
                 <h3 className={styles.h3}>Aditya Pratap Singh Sengar</h3>
-                <p>General Secretary SAC
-                    IIIT NAGPUR
-                    gs@iiitn.ac.in
-                    9111391478</p>
+                <p>General Secretary SAC</p>
+                <p>IIIT NAGPUR</p>
+                <p>gs@iiitn.ac.in</p>
+                <p>9111391478</p>
             </div>
         </div>
         <div className={styles.address}>
             <h3 className={styles.h3}>Address:</h3>
-            <p>Survey No. 140,141/1 behind Br. Sheshrao Wankhade Shetkari Sahkari Soot Girni, Village - Waranga,
-                PO
-                -
-                Dongargaon(Butibori), Tahsil- Nagpur (Rural), District Nagpur, Maharashtra- 441108</p>
+            <p> Survey No. 140,141/1 behind Br. Sheshrao </p> 
+            <p> Wankhade Shetkari Sahkari Soot Girni, </p>
+            <p> Village - Waranga, PO - </p>
+            <p> Dongargaon(Butibori), Tahsil- Nagpur </p>
+            <p> (Rural), District Nagpur, Maharashtra- </p> 
+            <p> 441108 </p>
         </div>
       </div>
     </div>
