716eb747478a0b592db327d2516e3c1a52b36ee9
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-14 12:28:39 +0530

    add countdown and hero-footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,6 +12,57 @@ import TreeLeft from "./svgs/hero/TreeLeft";
 import TreeRight from "./svgs/hero/TreeRight";
 import Sun from "./svgs/hero/Sun";
 import DigitalTree from "./svgs/hero/DigitalTree";
+import HeroFooter from "./HeroFooter";
+// import Countdown from "react-countdown";<
+import dynamic from "next/dynamic";
+
+const Countdown = dynamic(import("react-countdown"), {
+  ssr: false,
+});
+
+const renderer = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+  completed,
+}: {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  completed: boolean;
+}) => {
+  if (completed) {
+    return <h1 className={styles["countdown-heading"]}> LIVE </h1>;
+  } else {
+    const DaysString = days > 1 ? "Days" : "Day";
+    const HoursString = hours > 1 ? "Hours" : "Hour";
+    const MinsString = minutes > 1 ? "Minutes" : "Minute";
+    const SecString = seconds > 1 ? "Seconds" : "Second";
+
+    return (
+      <div className={styles["countdown"]}>
+        <div className={styles["days"]}>
+          <h1 className={styles["countdown-heading"]}>{days}</h1>
+          <p className={styles["countdown-para"]}> {DaysString} </p>
+        </div>
+        <div className={styles["hours"]}>
+          <h1 className={styles["countdown-heading"]}>{hours}</h1>
+          <p className={styles["countdown-para"]}> {HoursString} </p>
+        </div>
+        <div className={styles["minutes"]}>
+          <h1 className={styles["countdown-heading"]}>{minutes}</h1>
+          <p className={styles["countdown-para"]}> {MinsString} </p>
+        </div>
+        <div className={styles["seconds"]}>
+          <h1 className={styles["countdown-heading"]}>{seconds}</h1>
+          <p className={styles["countdown-para"]}> {SecString} </p>
+        </div>
+      </div>
+    );
+  }
+};
 
 const Hero = () => {
   return (
@@ -46,7 +97,14 @@ const Hero = () => {
           </div>
           <div className={styles["underline"]}></div>
           <div className={styles["timing"]}>
-            <p>Oct 15-16</p>
+            {/* <p>going live in ... </p> */}
+          </div>
+          <div>
+            <Countdown
+              // new Date("October 15, 2022 10:00:00")
+              date={"October 15, 2022 10:00:00"}
+              renderer={renderer}
+            />
           </div>
         </div>
       </div>
@@ -106,6 +164,9 @@ const Hero = () => {
             <DigitalTree></DigitalTree>
           </div>
         </div>
+      </div>
+      <div className="hero-footer">
+        <HeroFooter />
       </div>
     </div>
   );
