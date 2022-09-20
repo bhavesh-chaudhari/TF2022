1716c1d76d7f0c4f433bae2a27360ae3ffd25e2a
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-20 15:50:41 +0530

    add viewcounter

--- src/components/ViewCounter.tsx
+++ src/components/ViewCounter.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect } from "react";
+import useSWR, { useSWRConfig } from "swr";
+import fetcher from "../utils/fetcher";
 
 const ViewCounter = () => {
-  return <div>ViewCounter</div>;
+  const { mutate } = useSWRConfig();
+  const { data } = useSWR<{ views: number }>(`/api/v1/views`, fetcher);
+  const views = data?.views;
+
+  useEffect(() => {
+    const registerView = () => {
+      fetch(`/api/v1/views`, {
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const newViews = data.views;
+          mutate(`/api/v1/views`, { ...data, views: newViews }, false);
+        });
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      registerView();
+    }
+  }, [mutate]);
+
+  return <span> {`${views! > 0 ? views!.toLocaleString() : "***"}`}</span>;
 };
 
 export default ViewCounter;
