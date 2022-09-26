1d4fa9ecb0023e69282f7983a4e4427fcc298500
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2022-09-26 20:38:04 +0530

    about-tantra

--- src/components/AboutTantra.tsx
+++ src/components/AboutTantra.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "../styles/AboutTantra.module.css";
 
 const AboutTantra = () => {
-  return <div className={styles["about-tantra"]}>AboutTantra</div>;
+  return <div className={styles["about-tantra"]}>
+    <div className={styles["heading"]}>ABOUT TANTRAFIESTA<hr className={styles["horizontalline"]} /></div>
+    <div className={styles["content"]}>Tantrafiesta is the annual science and technology festival of the Indian Institute of Information Technology, Nagpur.
+Tantrafiesta hosts a plethora of social initiatives and outreach programs such as hackathons, coding contests, robo-races, debates, and seminars that help inspire the next generation of scientists and engineers.
+
+Tantrafiesta aims to provide a platform in order to establish a forum that will enable the exchange of knowledge, experience, and information related to a range of emerging technologies. This year, TantaFiesta is focusing on the use of greener technologies, and how utilizing greener technologies is the way forward.
+</div>
+  </div>;
 };
 
 export default AboutTantra;
