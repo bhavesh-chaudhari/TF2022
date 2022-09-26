8505538e12b5faaf5b752495e9538df75cd18855
Pinchu2002 <priyanshuchhabda3@gmail.com>
2022-09-26 18:20:29 +0530

    Added Events and Speakers

--- src/components/HighlightedEvents.tsx
+++ src/components/HighlightedEvents.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import styles from "../styles/HighlightedEvents.module.css";
+import hackme from "../images/hackme.png";
+import robo from "../images/robo-rumble.png";
 
 const HighlightedEvents = () => {
-  return <div className={styles["highlighted-events"]}>HighlightedEvents</div>;
+  return (<div className={styles["highlighted-events"]}>
+    <div className={styles.header}>
+      <div className={styles.h1}><h1>EVENTS IN TF2022</h1></div>
+      <div className={styles.line}></div>
+    </div>
+    <div className={styles.components}>
+      <div className={styles.img1}>
+        <img src={hackme.src} alt="hackme" className={styles.logo}/>
+      </div>
+      <div className={styles.img2}>
+        <img src={robo.src} alt="robo-rumble" className={styles.logo}/>
+      </div>
+      <div className={styles.img3}>
+        <img src={hackme.src} alt="hackme" className={styles.logo}/>
+      </div>
+    </div>
+
+  </div>);
 };
 
 export default HighlightedEvents;
