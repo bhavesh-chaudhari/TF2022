2898656600030af77b9ebed90f56e4d3c8cb43e4
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-26 19:08:24 +0530

    fix styles in highlighted events

--- src/components/HightlightedSpeakers.tsx
+++ src/components/HightlightedSpeakers.tsx
@@ -4,24 +4,27 @@ import hackme from "../images/hackme.png";
 import robo from "../images/robo-rumble.png";
 
 const HighlightedSpeakers = () => {
-  return (<div className={styles["highlighted-events"]}>
-    <div className={styles.header}>
-      <div className={styles.h1}><h1>SPEAKERS AT TF2022</h1></div>
-      <div className={styles.line}></div>
+  return (
+    <div className={styles["highlighted-events"]}>
+      <div className={styles.header}>
+        <div className={styles.h1}>
+          <h1>SPEAKERS AT TF2022</h1>
+        </div>
+        <div className={styles.line}></div>
+      </div>
+      <div className={styles.components}>
+        <div className={styles.img1}>
+          <img src={hackme.src} alt="hackme" className={styles.logo} />
+        </div>
+        <div className={styles.img2}>
+          <img src={robo.src} alt="robo-rumble" className={styles.logo} />
+        </div>
+        <div className={styles.img3}>
+          <img src={hackme.src} alt="hackme" className={styles.logo} />
+        </div>
+      </div>
     </div>
-    <div className={styles.components}>
-      <div className={styles.img1}>
-        <img src={hackme.src} alt="hackme" className={styles.logo}/>
-      </div>
-      <div className={styles.img2}>
-        <img src={robo.src} alt="robo-rumble" className={styles.logo}/>
-      </div>
-      <div className={styles.img3}>
-        <img src={hackme.src} alt="hackme" className={styles.logo}/>
-      </div>
-    </div>
-
-  </div>);
+  );
 };
 
 export default HighlightedSpeakers;
