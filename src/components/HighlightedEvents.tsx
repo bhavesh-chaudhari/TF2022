865d9c2f29d872a6ecfc6a5975ec255f3b611c09
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-27 04:56:23 +0530

    fix about,events and speakers mobile view

--- src/components/HighlightedEvents.tsx
+++ src/components/HighlightedEvents.tsx
@@ -7,22 +7,12 @@ import cf from "../images/codefiesta.png";
 const HighlightedEvents = () => {
   return (
     <div className={styles["highlighted-events"]}>
-      <div className={styles["header"]}>
-        <div className={styles["h1"]}>
-          <h1>EVENTS IN TF2022</h1>
-        </div>
-        <div className={styles["line"]}></div>
-      </div>
+      <h1 className={styles["heading"]}>EVENTS IN TF2022</h1>
+      <div className={styles["underline"]}></div>
       <div className={styles["components"]}>
-        <div className={styles["img"]}>
-          <img src={hackme.src} alt="hackme" className={styles["logo"]} />
-        </div>
-        <div className={styles["img"]}>
-          <img src={robo.src} alt="robo-rumble" className={styles["logo"]} />
-        </div>
-        <div className={styles["img"]}>
-          <img src={cf.src} alt="cf" className={styles["logo"]} />
-        </div>
+        <img src={hackme.src} alt="hackme" className={styles["logo"]} />
+        <img src={robo.src} alt="robo-rumble" className={styles["logo"]} />
+        <img src={cf.src} alt="cf" className={styles["logo"]} />
       </div>
     </div>
   );
