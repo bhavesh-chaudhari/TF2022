41fe5f1fc306dd8c25443924af023e5e4c79fbac
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-09-27 19:27:16 +0530

    add hover over event and style fixes of TFinPast

--- src/components/HighlightedEvents.tsx
+++ src/components/HighlightedEvents.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NextImage from "next/image";
 import styles from "../styles/HighlightedEvents.module.css";
 import hackme from "../images/hackme.png";
 import robo from "../images/robo-rumble.png";
@@ -10,9 +11,15 @@ const HighlightedEvents = () => {
       <h1 className={styles["heading"]}>EVENTS IN TF2022</h1>
       <div className={styles["underline"]}></div>
       <div className={styles["components"]}>
-        <img src={hackme.src} alt="hackme" className={styles["logo"]} />
-        <img src={robo.src} alt="robo-rumble" className={styles["logo"]} />
-        <img src={cf.src} alt="cf" className={styles["logo"]} />
+        <a href="/events/codefiesta" className={styles["logo-container"]}>
+          <NextImage src={cf} className={styles["logo"]} alt="cf" />
+        </a>
+        <a href="/events/hackme" className={styles["logo-container"]}>
+          <NextImage src={hackme} className={styles["logo"]} alt="hackme" />
+        </a>
+        <a href="/events/robo-rumble" className={styles["logo-container"]}>
+          <NextImage src={robo} className={styles["logo"]} alt="robo-rumble" />
+        </a>
       </div>
     </div>
   );
